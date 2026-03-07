2ae301d725384a05b40b0384e28a71a299467522
Hydrolium <0627juna@gmail.com>
2026-03-08 03:41:51 +0900

    bug :(

--- scripts/manager/sword_manager.ts
+++ scripts/manager/sword_manager.ts
@@ -1,51 +1,35 @@
-import { ContextType, FoundSwordsContext, GameContext, SwordContext } from '../other/context.js';
+import { DB } from '../data/data.js';
+import { ContextType, FoundSwordsContext, SwordContext } from '../other/context.js';
 import { Observer, Piece, PieceItem, Sword, SwordInfoByPiece, SwordItem, SwordTestResult, UnknownItem } from '../other/entity.js';
-import { Game } from '../other/main.js';
-import { StatID } from './stat_manager.js';
+import { DeveloperMode } from '../screen/developer_mode.js';
+import { InventoryManager } from './inventory_manager.js';
+// import { Game } from '../other/main.js';
+import { StatID, StatManager } from './stat_manager.js';
 
 export class SwordManager extends Observer {
 
-    private readonly _swords: readonly Sword[];
-
     private _current_sword_index: number = 0;
-    private readonly _swordCount: number;
-    private readonly _pieceInfos: ReadonlyMap<String, readonly SwordInfoByPiece[]>;
 
     private readonly _foundSwordIndexes = new Set<number>();
 
-    public readonly maxUpgradableIndex: number;
-
-    constructor(swords?: readonly Sword[]) {
+    constructor(
+        private readonly _db: DB) {
         super();
-        this._swords = swords ?? [];
-
-        this.maxUpgradableIndex = this._swords.length -1;
-        this._swordCount = this._swords.length;
-
-        const pieceInfos: Map<String, SwordInfoByPiece[]> = new Map();
-        
-        for(const sword of this._swords) {
-            for(const piece of sword.pieces) {
-                const swordInfo = new SwordInfoByPiece(sword.id, sword.index, sword.name, sword.imgSrc, piece.prob, piece.maxDrop);
-
-                if(pieceInfos.has(piece.id)) pieceInfos.get(piece.id)?.push(swordInfo);
-                else pieceInfos.set(piece.id, [swordInfo]);
-            }
-        }
-
-        this._pieceInfos = pieceInfos;
-
     }
 
     public get current_sword_index(): number {
         return this._current_sword_index;
     }
 
-    public set current_sword_index(value: number) {
-        value = Math.min(Math.max(value, 0), this.maxUpgradableIndex);
+    private set current_sword_index(value: number) {
+        value = Math.min(Math.max(value, 0), this._db.maxSwordIndex);
         this._current_sword_index = value;
 
         this.notify(this.swordContext);
+    }
+
+    public notifyContext(context: SwordContext) {
+
     }
 
     public get swordContext(): SwordContext {
@@ -53,36 +37,36 @@ export class SwordManager extends Observer {
             type: ContextType.SWORD,
             index: this._current_sword_index,
             sword: this.getCalculatedCurrentSword(),
-            isMax: this._current_sword_index >= this.maxUpgradableIndex
+            isMax: this._current_sword_index >= this._db.maxSwordIndex
         };
     }
 
     public get foundSwordsContext(): FoundSwordsContext {
         const swords: Sword[] = [];
-        for(let i = 0; i < this._swordCount; i++) {
-            if(this._foundSwordIndexes.has(i)) swords.push(this.getSwordWithIdx(i));
+        for(let i = 0; i < this._db.maxSwordCount; i++) {
+            if(this._foundSwordIndexes.has(i)) swords.push(this._db.getSwordWithIdx(i));
         }
 
         return {
             type: ContextType.FOUND_SWORDS,
-            swords: this._swords,
+            swords: this._db.swordData,
             founds: this._foundSwordIndexes
         }
     }
 
     public getSwordsByPieceId(pieceId: string): readonly SwordInfoByPiece[] {
-        return this._pieceInfos.get(pieceId) ?? [];
+        return this._db.pieceInfos.get(pieceId) ?? [];
     }
 
 
     public getSwordWithId(id: string): Sword {
-        const res = this._swords.find(sword => sword.id == id)
+        const res = this._db.swordData.find(sword => sword.id == id)
         if(res === undefined) throw new Error(`Sword with ID ${id} not found.`);
         return res;
     }
 
     public getSwordWithIdx(index: number): Sword {
-        return this._swords[index];
+        return this._db.swordData[index];
     }
 
     public getCalculatedSwordWithId(id: string): Sword {
@@ -118,25 +102,22 @@ export class SwordManager extends Observer {
     }
 
     public getIndex(id: string): number {
-        return this._swords.indexOf(this.getSwordWithId(id));
+        return this._db.swordData.indexOf(this.getSwordWithId(id));
     }
 
     public calculateLoss(index: number): number {
-        return this._swords.slice(0, index+1).reduce((pre, cur) => pre += Game.statManager.calculate(StatID.SMITH, cur.cost), 0);
+        return this._db.swordData.slice(0, index+1).reduce((pre, cur) => pre += Game.statManager.calculate(StatID.SMITH, cur.cost), 0);
     }
 
     public isFound(value: number | string): boolean {
         switch (typeof value) {
             case "number": return this._foundSwordIndexes.has(value);
-            case "string": return this._foundSwordIndexes.has(this.getSwordWithId(value).index);
+            case "string": return this._foundSwordIndexes.has(this._db.getSwordWithId(value).index);
         }
     }
 
-    public findSword(index: number) {
-        if(this.isFound(index)) return;
-
+    public addToFound(index: number) {
         this._foundSwordIndexes.add(index);
-        Game.statManager.addStatPoint();
     }
 
     public getFoundSwordIndexes() : ReadonlySet<number> {
@@ -145,9 +126,9 @@ export class SwordManager extends Observer {
 
     public jumpTo(index: number) {
 
-        index = Math.min(Math.max(index, 0), this.maxUpgradableIndex);
+        index = Math.min(Math.max(index, 0), this._db.maxSwordIndex);
 
-        this.findSword(index);
+        // this.findSword(index);
 
         this.current_sword_index = index;
     }
@@ -160,32 +141,50 @@ export class SwordManager extends Observer {
     }
 
     public getCurrentSword(): Sword {
-        return this.getSwordWithIdx(this.current_sword_index);
+        return this._db.getSwordWithIdx(this.current_sword_index);
     }
 
-    public getCalculatedCurrentSword(): Sword {
-        return this.getCalculatedSwordWithIdx(this.current_sword_index);
-    }
+    // public getCalculatedCurrentSword(): Sword {
+    //     return this._db.getCalculatedSwordWithIdx(this.current_sword_index);
+    // }
 
     public getNextSword(): Sword {
-        return this.getSwordWithIdx(Math.min(this.current_sword_index +1, this.maxUpgradableIndex));
+        return this._db.getSwordWithIdx(Math.min(this.current_sword_index +1, this._db.maxSwordIndex));
     }
 
-    public tryUpgrade(): {result: SwordTestResult, result_index: number, sword?: Sword, dropped_pieces?: readonly PieceItem[]} {
+    public tryUpgrade(
+        inventoryManager: InventoryManager,
+        developerMode: DeveloperMode,
+        statManager: StatManager
+    ): {result: SwordTestResult, result_index: number, sword?: Sword, dropped_pieces?: readonly PieceItem[]} {
         
-        const sword = this.getCalculatedCurrentSword();
+        // const sword = this.getCalculatedCurrentSword();
 
-        if(this.current_sword_index >= this.maxUpgradableIndex) return {result: SwordTestResult.REJECTED_BY_MAX_UPGRADE, result_index: this.current_sword_index};
-        if(!Game.inventoryManager.hasMoney(sword.cost)) return {result: SwordTestResult.REJECTED_BY_MONEY_LACK, result_index: this.current_sword_index};
+        let sword = this._db.getSwordWithIdx(this.current_sword_index);
+        sword = new Sword(
+            sword.id,
+            sword.index,
+            sword.name,
+            sword.imgSrc,
+            statManager.calculate(StatID.LUCKY_BRACELET, sword.prob),
+            statManager.calculate(StatID.SMITH, sword.cost),
+            statManager.calculate(StatID.BIG_MERCHANT, sword.price),
+            sword.requiredRepairs,
+            sword.canSave,
+            sword.pieces.map(piece => new Piece(piece.id, piece.name, piece.imgSrc, piece.prob, statManager.calculate(StatID.MAGIC_HAT, piece.maxDrop)))
+        );
 
-        Game.inventoryManager.takeMoney(sword.cost, {
+        if(this.current_sword_index >= this._db.maxSwordIndex) return {result: SwordTestResult.REJECTED_BY_MAX_UPGRADE, result_index: this.current_sword_index};
+        if(!inventoryManager.hasMoney(sword.cost)) return {result: SwordTestResult.REJECTED_BY_MONEY_LACK, result_index: this.current_sword_index};
+
+        inventoryManager.takeMoney(sword.cost, {
             type: ContextType.SWORD_UPGRADE,
             name: sword.name,
             cost: sword.cost
         });
 
-        if(Game.developerMod.alwaysSuccess || Math.random() < sword.prob) {
-            if(Math.random() < Game.statManager.calculate(StatID.GOD_HAND)) {
+        if(developerMode.alwaysSuccess || Math.random() < sword.prob) {
+            if(Math.random() < statManager.calculate(StatID.GOD_HAND)) {
                 this.upgradeSword(2);
                 return {result: SwordTestResult.GREAT_SUCCESS, result_index: this.current_sword_index};
             } else {
@@ -197,12 +196,12 @@ export class SwordManager extends Observer {
                 piece => piece.drop()
             ).filter(piece => piece.count > 0);
 
-            dropped_pieces.forEach(piece => Game.inventoryManager.save(piece));
+            dropped_pieces.forEach(piece => inventoryManager.save(piece));
 
-            if(Math.random() < Game.statManager.calculate(StatID.INVALIDATED_SPHERE)/100 ) {
+            if(Math.random() < statManager.calculate(StatID.INVALIDATED_SPHERE)/100 ) {
                 this.upgradeSword(0);
 
-                Game.inventoryManager.saveMoney(sword.cost, {
+                inventoryManager.saveMoney(sword.cost, {
                     type: ContextType.SWORD_RESTORE,
                     name: sword.name,
                     cost: sword.cost
