679e18ec27e7fd4e2f2f0d7ab63d9da245722027
Hydrolium <0627juna@gmail.com>
2026-03-15 03:51:09 +0900

    how_to_play.html 에 vite가 css 경로 수정을 못하는 버그 수정

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,12 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   build: {
     outDir: 'docs',
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        howToPlay: 'how_to_play.html'
+      }
+    }
   },
   base: '/Strengthening_sword/'
 })
